e25ccda98f5484f53a5677318e2b8c2cc22119ee
youssefrhanem <youssefrhanem91@gmail.com>
2023-09-06 11:15:10 +0100

    Initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+
+
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  exports: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
